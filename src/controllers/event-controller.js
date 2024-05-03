6cf88c13379dc89f4d21c3e376ef07a2ada0de76
VALENTINA ORIT VUGIN <47866545@ort>
2024-05-03 11:42:41 -0300

    avanzando con el controler

--- src/controllers/event-controller.js
+++ src/controllers/event-controller.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
+// 3 Busqueda de un evento
 router.get("", (req, res) => {
     const response = {
         name: null,
@@ -15,5 +16,30 @@ router.get("", (req, res) => {
 
     res.status(200).send(response);
 })
+
+// 4 Detalle de un evento
+router.get(":id", (req, res) => {
+    const id = req.params.id;
+    res.status(200).send(response);
+})
+
+// 5 Listado de participantes
+router.get(":id/enrollment", (req, res) => {
+    const id = req.params.id;
+    const response = {
+        first_name: null,
+        last_name: null,
+        username: null,
+        attended: null,
+        rating: null
+    }
+
+    for (const [key, value] of Object.entries(req.query)) {
+        if(response[`${key}`] !== undefined) response[`${key}`] = value;
+    }
+
+    res.status(200).send(response);
+})
+
 
 export default router;

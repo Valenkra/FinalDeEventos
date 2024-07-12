f3b1336f7a3b8c45053d28ee788540c8ce4058c5
Android Studio <devandroid@ort>
2024-07-12 12:08:24 -0300

    db_finished

--- src/database/stored_procedures.sql
+++
@@ -1,6 +0,0 @@
-CREATE PROCEDURE BuscarEvento(name character varying, category character varying, startdate date, tag character varying)
-LANGUAGE SQL
-BEGIN ATOMIC
-  SELECT * FROM public.events
-	INNER JOIN event_categories EC ON public.events.id_event_category = public.event_categories.id
-END;
